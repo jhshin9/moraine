9de9c63441912c6ac7fc5d2ec9e25b5a769cc93c
junghun <jhshin9@gmail.com>
2015-10-29 08:38:00 -0600

    Start

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Moraine Server.
+ *
+ * @author Jackson
+ * @type {*|exports|module.exports}
+ */
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello Moraine')
+})
+
+app.listen(8000)
