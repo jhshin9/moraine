76b77580a05dc000303a6ee1852ae2eac549f5f1
junghun <jhshin9@gmail.com>
2015-10-30 08:00:24 -0600

    add the cluster module.

--- server.js
+++ server.js
@@ -5,10 +5,26 @@
  * @type {*|exports|module.exports}
  */
 var express = require('express');
+var cluster = require('cluster');
 var app = express();
+
+var numCPUs = require('os').cpus().length;
 
 app.get('/', function (req, res) {
     res.send('Hello Moraine');
 });
 
-app.listen(8000);
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    app.listen(8000);
+}
